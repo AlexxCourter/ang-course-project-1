d51b9597ae0d542a7253d772e30a18a373bfcb7d
AlexxCourter <alex.courter@gmail.com>
2024-05-02 13:17:01 -0400

    mod3 course project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Chocolate Milk", "Chocolate syrup mixed into milk.", "https://images.unsplash.com/photo-1585752379769-801711aaf836?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
