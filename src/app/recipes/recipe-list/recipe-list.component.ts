6b5677bf4e5a6131376dd2a4896dc6ab84a76860
AlexxCourter <alex.courter@gmail.com>
2024-05-11 20:34:00 -0400

    added recipe/shopping list dynamic data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("Chocolate Milk", "Chocolate syrup mixed into milk.", "https://images.unsplash.com/photo-1585752379769-801711aaf836?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    new Recipe("Chocolate Milk", "Chocolate syrup mixed into milk.", "https://images.unsplash.com/photo-1585752379769-801711aaf836?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    new Recipe("Yakisoba", "Japanese style fried noodles, chicken, and vegetables.", "https://plus.unsplash.com/premium_photo-1664475934279-2631a25c42ce?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
   ];
 
   constructor() {
@@ -17,5 +19,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, ViewChild, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    const newIngredientName = this.nameInputRef.nativeElement.value;
+    const newIngredientAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(newIngredientName, newIngredientAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
